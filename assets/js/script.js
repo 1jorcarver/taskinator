447e80f2334f5fcfe5a9b6b41e478a4c716d8a7b
Jordan Carver <jordan_carver@hotmail.com>
2020-08-22 12:12:50 -0600

    added draggable Element

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,3 +206,25 @@ var dropZoneDragHandler = function(event) {
 };
 
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+
+var dropTaskHandler = function(event) {
+    var id = event.dataTransfer.getData("text/plain");
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+    
+    if (statusType === "tasks-to-do") {
+        statusSelectEl.selectedIndex = 0;
+    }
+    else if (statusType === "tasks-in-progress") {
+        statusSelectEl.selectedIndex = 1;
+    }
+    else if (statusType === "tasks-completed") {
+        statusSelectEl.selectedIndex = 2;
+    };
+
+    dropZoneEl.appendChild(draggableElement);
+};
+
+pageContentEl.addEventListener("drop", dropTaskHandler);
